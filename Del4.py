9e024ff87e9062686dc09a45a6e69d1602dbfee5
ceafagerlund <31474765+ceafagerlund@users.noreply.github.com>
2017-09-01 16:17:45 +0200

    Update Del4.py

--- Del4.py
+++ Del4.py
@@ -1,8 +1,9 @@
 n = int(input("Ge mig siffror!! "))
 a = 0
-b = (1729-(a**3))**(1/3)
+b = (n-(a**3))**(1/3)
 while (a**3 + b**3 != n):
  a = a + 1
  print((a**3)+(b**3))
 if ((a**3)+(b**3)==n):
+ break
  print (a, b)
